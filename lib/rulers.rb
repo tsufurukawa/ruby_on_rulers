0e9f6c6b2be023af72d4fbd822d7d181133d82ef
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-11-09 23:13:14 -0800

    Add simple exception handling and root routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,13 +6,21 @@ module Rulers
     def call(env)
       # TODO: Fix this "hack" for handling favicon
       if env["PATH_INFO"] == "/favicon.ico"
-        return [404, {'Content-Type' => 'text/html'}, []]
+        return [404, {"Content-Type" => "text/html"}, []] 
+      elsif env["PATH_INFO"] == "/"
+        path = File.expand_path("../../public/root.html", __FILE__)
+        html = File.read(path)
+        return [200, {"Content-Type" => "text/html"}, [html]]
       end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      begin
+        text = controller.send(act)
+      rescue
+        return [500, {"Content-Type" => "text/html"}, ["Oops, something went wrong!!"]]
+      end
+      [200, {"Content-Type" => "text/html"}, [text]]
     end
   end
 
